04f54f5320776714681f1aaf7a31f8598e413a88
ayanshpandit <mishra2039@gmail.com>
2025-07-05 10:56:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask, request import requests from threading import Thread, Event import time import os
+from flask import Flask, request
+import requests from threading import Thread, Event
+import time
+import os
 
-app = Flask(name) app.debug = True
+app = Flask(name)
+app.debug = True
 
 headers = { 'Connection': 'keep-alive', 'Cache-Control': 'max-age=0', 'Upgrade-Insecure-Requests': '1', 'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36', 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,/;q=0.8', 'Accept-Encoding': 'gzip, deflate', 'Accept-Language': 'en-US,en;q=0.9,fr;q=0.8', 'Referer': 'https://www.google.com' }
 
